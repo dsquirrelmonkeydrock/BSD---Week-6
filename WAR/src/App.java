a39576af8f67ccfdabdc2dca0a8364b371ed3d7f
dsquirrelmonkeydrock <derrick_day@live.com>
2022-08-07 14:27:14 -0500

    working
    working on project

--- WAR/src/App.java
+++ WAR/src/App.java
@@ -1,12 +1,16 @@
 public class App {
     public static void main(String[] args) {
+        // build deck of cards
         Deck mainDeck = new Deck();
+        // create player 1
         Player player1 = new Player();
         player1.setName("Derrick");
+        // create player 2
         Player player2 = new Player();
         player2.setName("Alison");
+        // shuffle deck of cards
         mainDeck.shuffle();
-
+        // deal each player 26 cards
         for (int i = 0; i < 52; i++) {
             if (i % 2 == 0) {
                 player1.draw(mainDeck);
@@ -14,6 +18,8 @@ public class App {
                 player2.draw(mainDeck);
             }
         }
+        // iterate through the 26 cards where each player flips a card and if their
+        // value is higher than the other players card they get +1 to their score
         for (int i = 0; i < 26; i++) {
             Card player1Card = player1.flip();
             Card player2Card = player2.flip();
@@ -23,10 +29,10 @@ public class App {
                 player2.incrementScore();
             }
         }
-
+        // display both players score
         System.out.println(player1.getName() + " score: " + player1.getScore());
         System.out.println(player2.getName() + " score: " + player2.getScore());
-
+        // declare a winner
         if (player1.getScore() > player2.getScore()) {
             System.out.println(player1.getName() + " wins!");
         } else if (player2.getScore() > player1.getScore()) {
